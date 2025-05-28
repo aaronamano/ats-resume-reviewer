a5055fd7de2c729501ce9db2d5204167e0ec80ae
aaronamano <aaroncedric2005@yahoo.com>
2025-05-27 20:39:53 -0400

    installed cors and about to start on the backend

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,31 @@
 from fastapi import FastAPI, UploadFile, File
 from PyPDF2 import PdfReader
 import io
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-@app.post("/upload-pdf")
+@app.post("/analyze")
+#extract text from PDF file
 async def extract_text_from_pdf(pdf_file: UploadFile = File(...)):
     try:
         # Read the uploaded file
@@ -25,3 +42,12 @@ async def extract_text_from_pdf(pdf_file: UploadFile = File(...)):
         return {"text": text}
     except Exception as e:
         return {"error": str(e)}
+    
+#convert extracted text to vector embeddings
+#send vector embeddings to vector database
+#conduct vector search to find similar keywords between job description and resume
+#return results with similarity scores
+
+
+#extract job description and convert to vector embeddings
+#store vector embeddings in vector database
